908428550939acafb7d60a5b0d385ce652f0f2e9
Ivan Krysak <ivan@rev.ng>
2022-01-19 17:01:43 +0200

    Specialize `CallingConventionTrait` for x86

---
+++ include/revng/ABI/Specializations/Microsoft_x86_cdecl.h
@@ -0,0 +1,48 @@
+#pragma once
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "revng/Model/ABI.h"
+#include "revng/Model/Register.h"
+
+template<model::ABI::Values ABI>
+struct CallingConventionTrait;
+
+template<>
+struct CallingConventionTrait<model::ABI::Microsoft_x86_cdecl> {
+  static constexpr auto ABI = model::ABI::Microsoft_x86_cdecl;
+
+  static constexpr bool ArgumentsArePositionBased = false;
+  static constexpr bool OnlyStartDoubleArgumentsFromAnEvenRegister = false;
+  static constexpr bool ArgumentsCanBeSplitBetweenRegistersAndStack = false;
+  static constexpr bool UsePointerToCopyForStackArguments = false;
+
+  static constexpr size_t MaximumGPRsPerAggregateArgument = 0;
+  static constexpr size_t MaximumGPRsPerAggregateReturnValue = 0;
+  static constexpr size_t MaximumGPRsPerScalarArgument = 0;
+  static constexpr size_t MaximumGPRsPerScalarReturnValue = 2;
+
+  static constexpr std::array<model::Register::Values, 0>
+    GeneralPurposeArgumentRegisters = {};
+  static constexpr std::array GeneralPurposeReturnValueRegisters = {
+    model::Register::eax_x86,
+    model::Register::edx_x86
+  };
+
+  static constexpr std::array<model::Register::Values, 0>
+    VectorArgumentRegisters = {};
+  static constexpr std::array<model::Register::Values, 0>
+    VectorReturnValueRegisters = {};
+
+  static constexpr std::array CalleeSavedRegisters = {
+    model::Register::ebx_x86,
+    model::Register::ebp_x86,
+    model::Register::esp_x86,
+    model::Register::edi_x86,
+    model::Register::esi_x86
+  };
+
+  static constexpr auto ReturnValueLocationRegister = model::Register::eax_x86;
+};
