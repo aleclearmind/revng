5a69d66eee21fbd8165ef4f9d7e5d3545925b379
Ivan Krysak <ivan@rev.ng>
2023-11-28 12:17:51 +0100

    ABI: expose argument distribution ABI
    This allows external code to peek inside a layout and to tell how
    a specific argument would be treated based on its type.

--- lib/ABI/FunctionType/Layout.cpp
+++ lib/ABI/FunctionType/Layout.cpp
@@ -15,64 +15,11 @@
 #include "revng/Model/Helpers.h"
 #include "revng/Support/Debug.h"
 
+#include "Distribution.h"
+
 static Logger Log("function-type-conversion-to-raw");
 
 namespace abi::FunctionType {
-
-using RegisterVector = llvm::SmallVector<model::Register::Values, 2>;
-
-/// The internal representation of the argument shared between both
-/// the to-raw conversion and the layout.
-struct DistributedArgument {
-  /// A list of registers the argument uses.
-  RegisterVector Registers = {};
-
-  /// The total size of the argument (including padding if necessary) in bytes.
-  uint64_t Size = 0;
-
-  /// The size of the piece of the argument placed on the stack.
-  /// \note: has to be equal to `0` or `this->Size` for any ABI for which
-  ///        `abi::Definition::ArgumentsCanBeSplitBetweenRegistersAndStack()`
-  ///        returns `false`. Otherwise, it has to be an integer value, such
-  ///        that `(0 <= SizeOnStack <= this->Size)` is true.
-  uint64_t SizeOnStack = 0;
-
-  /// Mark this argument as a padding argument, which means an unused location
-  /// (either a register or a piece of the stack) which needs to be seen as
-  /// a separate argument to be able to place all the following arguments
-  /// in the correct positions.
-  ///
-  /// The "padding" arguments are emitted as normal arguments in RawFunctionType
-  /// but are omitted in `Layout`.
-  bool RepresentsPadding = false;
-};
-using DistributedArguments = llvm::SmallVector<DistributedArgument, 8>;
-
-using RegisterSpan = std::span<const model::Register::Values>;
-using ArgumentSet = TrackingSortedVector<model::Argument>;
-
-class ArgumentDistributor {
-private:
-  const abi::Definition &ABI;
-
-public:
-  uint64_t UsedGeneralPurposeRegisterCount = 0;
-  uint64_t UsedVectorRegisterCount = 0;
-  uint64_t UsedStackOffset = 0;
-  uint64_t ArgumentIndex = 0;
-
-public:
-  explicit ArgumentDistributor(const abi::Definition &ABI) : ABI(ABI) {
-    revng_assert(ABI.verify());
-  }
-
-  DistributedArguments next(const model::QualifiedType &ArgumentType);
-
-private:
-  DistributedArguments positionBased(const model::QualifiedType &ArgumentType);
-  DistributedArguments
-  nonPositionBased(const model::QualifiedType &ArgumentType);
-};
 
 class ToRawConverter {
 private:
